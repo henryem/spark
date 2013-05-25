92aa7fa98c2864f01857cb9480e928a70038dfd0
Henry Milner <henry@conviva.com>
2013-05-24 17:14:25 -0700

    fixup

--- core/src/test/scala/spark/LocalSparkSuite.scala
+++ core/src/test/scala/spark/LocalSparkSuite.scala
@@ -10,31 +10,31 @@ import java.io.ObjectOutputStream
 import java.io.IOException
 
 class LocalSparkSuite extends FunSuite with ShouldMatchers {
-//  test("Use LocalSpark locally") {
-//    val data = (0 until 1000)
-//    val transform = {(num: Int) => num*2}
-//    val results = Await.result(LocalSpark.runLocally(data, {rdd: RDD[Int] => rdd.map(transform).collect}), Duration.Inf)
-//    results.deep should equal (data.map(transform))
-//  }
+  test("Use LocalSpark locally") {
+    val data = (0 until 1000)
+    val transform = {(num: Int) => num*2}
+    val results = Await.result(LocalSpark.runLocally(data, {rdd: RDD[Int] => rdd.map(transform).collect}), Duration.Inf)
+    results.deep should equal (data.map(transform))
+  }
   
-//  test("Use a new thread inside a Spark job") {
-//    val numTasks = 4
-//    val numReps = 8
-//    val sc = new SparkContext("local[%d]".format(numTasks), "test")
-//    val reps = sc.parallelize(0 until numReps, numTasks)
-//    val data = (0 until 100)
-//    val transform = {(num: Int) => num*2}
-//    val results = reps
-//      .map(rep => {
-//        val executor = Executors.newSingleThreadExecutor() //TMP
-//        val result = executor.submit(new Callable[Array[Int]]() { def call() = data.map(transform).toArray })
-//        executor.shutdown()
-//        result.get
-//      })
-//      .collect
-//    results.deep should equal ((0 until numReps).map(rep => data.map(transform)))
-//    sc.stop()
-//  }
+  test("Use a new thread inside a Spark job") {
+    val numTasks = 4
+    val numReps = 8
+    val sc = new SparkContext("local[%d]".format(numTasks), "test")
+    val reps = sc.parallelize(0 until numReps, numTasks)
+    val data = (0 until 100)
+    val transform = {(num: Int) => num*2}
+    val results = reps
+      .map(rep => {
+        val executor = Executors.newSingleThreadExecutor() //TMP
+        val result = executor.submit(new Callable[Array[Int]]() { def call() = data.map(transform).toArray })
+        executor.shutdown()
+        result.get
+      })
+      .collect
+    results.deep should equal ((0 until numReps).map(rep => data.map(transform)))
+    sc.stop()
+  }
   
   test("Use LocalSpark inside a Spark job") {
     val numTasks = 1
@@ -42,32 +42,30 @@ class LocalSparkSuite extends FunSuite with ShouldMatchers {
     val sc = LocalSpark.createLocalContext(1)
     val reps = sc.parallelize(0 until numReps, numTasks)
     val data = (0 until 100)
-    val results = reps
-      .map(new OuterMapper(data))
-      .collect
     val transform = {(num: Int) => num*2}
+    val results = reps
+      .map({rep =>  Await.result(LocalSpark.runLocally(data, {rdd: RDD[Int] => rdd.map(_*2).collect}), Duration.Inf)})
+      .collect
     results.deep should equal ((0 until numReps).map(rep => data.map(transform)))
     sc.stop()
   }
 }
 
-//TMP
-class OuterMapper(data: Seq[Int]) extends Function1[Int,Array[Int]] with Serializable {
-  override def apply(rep: Int) = {
-    println("Rep=%d".format(rep)) //TMP
-    val transformRdd = new Function1[RDD[Int], Array[Int]]{
-      def apply(rdd: RDD[Int]) = rdd.map(InnerMapper).collect
-      override def toString() = "transformRdd"
-    }
-    val results = Await.result(LocalSpark.runLocally(data, transformRdd), Duration.Inf)
-    results
+object LocalSparkSuite {
+  def main(args: Array[String]): Unit = {
+    val master = args(0)
+    val numTasks = args(1).toInt
+    val numReps = args(2).toInt
+    val dataSize = args(3).toInt
+    
+    val sc = LocalSpark.createLocalContext(1)
+    val reps = sc.parallelize(0 until numReps, numTasks)
+    val data = (0 until dataSize)
+    val transform = {(num: Int) => num*2}
+    val results = reps
+      .map({rep =>  Await.result(LocalSpark.runLocally(data, {rdd: RDD[Int] => rdd.map(transform).collect}), Duration.Inf)})
+      .collect
+    println("Results: %s".format(results.deep))
+    sc.stop()
   }
-      
-  override def toString() = "outerMapper"
-}
-
-//TMP
-object InnerMapper extends Function1[Int,Int] with Serializable {
-  override def apply(num: Int) = num*2
-  override def toString() = "transformRow"
 }
